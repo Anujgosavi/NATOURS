6ee02c5841260d31a86459e7deb21a9033c9f29d
Anuj Gosavi <anujgosavi2005@gmail.com>
2025-07-15 23:56:49 +0530

    Added reviews

---
+++ backend/models/reviewModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+// const slugify = require('slugify');
+// const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Rating cannot be empty']
+    },
+    crreatedAt: {
+      type: Date,
+      default: Date.now()
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour']
+    },
+    User: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
